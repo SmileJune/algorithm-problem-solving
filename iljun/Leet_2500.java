684c34054674772ce2e02396137e3e1b8eb2a31a
Smilejune <godhkekf24@inha.edu>
2023-09-08 22:42:37 +0900

    feat: 리트코드 2500번 문제 풀이

---
+++ iljun/Leet_2500.java
@@ -0,0 +1,45 @@
+package iljun;
+
+import java.util.Arrays;
+
+public class Leet_2500 {
+
+    public static void main(String[] args) {
+        System.out.println(deleteGreatestValue(new int[][]{{1, 2, 4}, {3, 3, 1}}));
+    }
+    public static int deleteGreatestValue(int[][] grid) {
+
+        // 오름차순 정렬
+        int[][] sorted_grid = new int[grid.length][grid[0].length];
+
+        for(int i = 0; i < grid.length; i++) {
+            int[] arr = grid[i];
+            Arrays.sort(arr);
+            sorted_grid[i] = arr;
+        }
+
+        // reverse matrix
+        int[][] reversed_grid = new int[grid[0].length][grid.length];
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                reversed_grid[j][i] = grid[i][j];
+            }
+        }
+
+        // 오름차순 정렬
+        int[][] sorted_reversed_grid = new int[reversed_grid.length][reversed_grid[0].length];
+
+        for(int i = 0; i < reversed_grid.length; i++) {
+            int[] arr = reversed_grid[i];
+            Arrays.sort(arr);
+            sorted_reversed_grid[i] = arr;
+        }
+
+        int max = 0;
+        for(int i = 0; i < reversed_grid.length; i++) {
+            max += reversed_grid[i][reversed_grid[i].length - 1];
+        }
+
+        return max;
+    }
+}

f94a607b3e0eda9f4bf8befbbf035b501697e43d
Smilejune <godhkekf24@inha.edu>
2023-10-06 22:43:14 +0900

    feat: 리트코드 20번 문제풀이

---
+++ iljun/leetcode/Q_20.java
@@ -0,0 +1,23 @@
+package iljun.leetcode;
+
+import java.util.*;
+
+class Q_20 {
+    public boolean isValid(String s) {
+        Stack<Character> leftSymbols = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                leftSymbols.push(c);
+            } else if (c == ')' && !leftSymbols.isEmpty() && leftSymbols.peek() == '(') {
+                leftSymbols.pop();
+            } else if (c == '}' && !leftSymbols.isEmpty() && leftSymbols.peek() == '{') {
+                leftSymbols.pop();
+            } else if (c == ']' && !leftSymbols.isEmpty() && leftSymbols.peek() == '[') {
+                leftSymbols.pop();
+            } else {
+                return false;
+            }
+        }
+        return leftSymbols.isEmpty();
+    }
+}
